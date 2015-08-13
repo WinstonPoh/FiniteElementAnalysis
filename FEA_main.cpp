b6ba51d56e6f23819f961605a44ea869539269af
Winston Poh <winst_940116@hotmail.my>
2015-08-14 01:36:30 +1200

    Added FEA_main.cpp and bar.cpp. Considering creating bar, beam, and frame objects to learn OOP.

---
+++ FEA_main.cpp
@@ -0,0 +1,16 @@
+/*
+ * FEA_main.cpp
+ *
+ *	A program using Finite Element Methods to solve for bar, beam, or frame element deflections.
+ *
+ *  Created on: Aug 14, 2015
+ *      Author: Winston Poh
+ */
+
+#include <iostream>
+#include <cmath>
+
+
+
+
+
---
+++ bar.cpp
@@ -0,0 +1,21 @@
+/*
+ * bar.cpp
+ *
+ *	Functions related to bars
+ *
+ *  Created on: Aug 14, 2015
+ *      Author: wpoh
+ */
+/*	Notes: Should try implementing classes for bar, beam, and frame elements.
+ *
+ */
+
+double E;
+double Area;
+double L;
+
+double* local_bar(E, Area, L) {
+	int const transforming_array[2][2] = {{  1 , -1 },
+										  { -1 ,  1 }};
+
+}
